15d6aec2104048bb19cbdd7c1a46a177562011f2
romainbenard <90315686+romainbenard@users.noreply.github.com>
2025-04-19 19:02:49 +0200

    add correct data

--- src/assets/data.js
+++ src/assets/data.js
@@ -2,109 +2,122 @@ export const homeData = {
   "/index.html": {
     title: "Isabelle Hautbois",
     quoteOne:
-      "Chaque toile est le résultat d’une petite cuisine me permettant de transcrire mon propre univers. Mes paysages intérieurs sont volontiers abstraits associant matières et couleurs.",
+      "Chaque toile est le résultat d'une petite cuisine me permettant de transcrire mon propre univers. Mes paysages intérieurs sont volontiers abstraits associant matières et couleurs.",
     quoteTwo:
-      "Aucune intention de départ, les teintes s’imposent au fur et à mesure. La construction se fait pas à pas. Certains endroits restent tels quels, fruits du premier geste ou toile vierge, d’autres sont totalement recouverts de couches successives de peinture, collage, pigments ou pastels gras. Inlassablement grattées, parfois retirées pour faire réapparaître une infime partie de la couche initiale, les strates se succèdent et le tableau prend forme.",
+      "Aucune intention de départ, les teintes s'imposent au fur et à mesure. La construction se fait pas à pas. Certains endroits restent tels quels, fruits du premier geste ou toile vierge, d'autres sont totalement recouverts de couches successives de peinture, collage, pigments ou pastels gras. Inlassablement grattées, parfois retirées pour faire réapparaître une infime partie de la couche initiale, les strates se succèdent et le tableau prend forme.",
     paintings: [
       [
         {
-          title: "Painting 1",
-          image: "https://picsum.photos/200",
+          title: "Le Semeur",
+          image: "/img/le-semeur.jpg",
           medium: "Oil on canvas",
         },
         {
-          title: "Painting 2",
-          image: "https://picsum.photos/300/200",
+          title: "Mini Mexico",
+          image: "/img/mini-mexico.jpg",
           medium: "Oil on canvas",
         },
         {
-          title: "Painting 3",
-          image: "https://picsum.photos/500/200",
-          medium: "Oil on canvas",
-        },
-      ],
-      [
-        {
-          title: "Painting 4",
-          image: "https://picsum.photos/400/300",
-          medium: "Oil on canvas",
-        },
-        {
-          title: "Painting 5",
-          image: "https://picsum.photos/400",
-          medium: "Oil on canvas",
-        },
-        {
-          title: "Painting 6",
-          image: "https://picsum.photos/175",
+          title: "Intersection",
+          image: "/img/intersection.jpg",
           medium: "Oil on canvas",
         },
       ],
       [
         {
-          title: "Painting 7",
-          image: "https://picsum.photos/400/600",
+          title: "Mini Grand Kimono",
+          image: "/img/mini-grand-kimono.jpg",
           medium: "Oil on canvas",
         },
         {
-          title: "Painting 8",
-          image: "https://picsum.photos/500",
+          title: "Fond Rouge",
+          image: "/img/sans_titre8.jpg",
           medium: "Oil on canvas",
         },
         {
-          title: "Painting 9",
-          image: "https://picsum.photos/200",
-          medium: "Oil on canvas",
-        },
-      ],
-      [
-        {
-          title: "Painting 10",
-          image: "https://picsum.photos/200/312",
-          medium: "Oil on canvas",
-        },
-        {
-          title: "Painting 11",
-          image: "https://picsum.photos/211",
-          medium: "Oil on canvas",
-        },
-        {
-          title: "Painting 12",
-          image: "https://picsum.photos/200",
+          title: "Bréhat",
+          image: "/img/sans_titre7.jpg",
           medium: "Oil on canvas",
         },
       ],
       [
         {
-          title: "Painting 13",
-          image: "https://picsum.photos/200/412",
+          title: "Pommier",
+          image: "/img/sans_titre5.jpg",
           medium: "Oil on canvas",
         },
         {
-          title: "Painting 14",
-          image: "https://picsum.photos/301/200",
+          title: "Le Pin",
+          image: "/img/sans_titre4.jpg",
           medium: "Oil on canvas",
         },
         {
-          title: "Painting 15",
-          image: "https://picsum.photos/198",
+          title: "Tête à Queue",
+          image: "/img/sans_titre2.jpg",
           medium: "Oil on canvas",
         },
       ],
       [
         {
-          title: "Painting 16",
-          image: "https://picsum.photos/199",
+          title: "Petit Kimono",
+          image: "/img/petit-kimono.jpg",
           medium: "Oil on canvas",
         },
         {
-          title: "Painting 17",
-          image: "https://picsum.photos/201",
+          title: "Oxygene",
+          image: "/img/oxygene.jpg",
           medium: "Oil on canvas",
         },
         {
-          title: "Painting 18",
-          image: "https://picsum.photos/202",
+          title: "Mini Orage",
+          image: "/img/mini-orage.jpg",
+          medium: "Oil on canvas",
+        },
+      ],
+      [
+        {
+          title: "Destination",
+          image: "/img/destination.jpg",
+          medium: "Oil on canvas",
+        },
+
+        {
+          title: "Mini La Lettre",
+          image: "/img/mini-la-lettre.jpg",
+          medium: "Oil on canvas",
+        },
+        {
+          title: "Clown Triste",
+          image: "/img/sans_titre6.jpg",
+          medium: "Oil on canvas",
+        },
+      ],
+      [
+        {
+          title: "Mini Arbre Boule",
+          image: "/img/mini-arbre-boule.jpg",
+          medium: "Oil on canvas",
+        },
+        {
+          title: "Malouines",
+          image: "/img/malouines.jpg",
+          medium: "Oil on canvas",
+        },
+        {
+          title: "La Faille",
+          image: "/img/la_faille.jpg",
+          medium: "Oil on canvas",
+        },
+        {
+          title: "Dame De Coeur",
+          image: "/img/dame_de_coeur.jpg",
+          medium: "Oil on canvas",
+        },
+      ],
+      [
+        {
+          title: "Grenade",
+          image: "/img/grenade.jpg",
           medium: "Oil on canvas",
         },
       ],
